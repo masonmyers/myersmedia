02718f5a111ffca1b56053f1d992ad457af6b9b5
masonmyers <32687116+masonmyers@users.noreply.github.com>
2026-08-17 07:42:50 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+window.SITE_CONFIG = {
+  GOOGLE_SHEETS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyqDvDokiuI9m4ub4HDQERlewou95diSdd_r7MssvJHJZgMrVhlznkf2SNK-kU7qtDF/exec",
+
+  SOCIAL_LINKS: {
+    twitter: "https://twitter.com/myersmediabiz",
+    linkedin: "https://www.linkedin.com/company/myersmediabiz",
+    instagram: "https://www.instagram.com/myersmediabiz"
+  }
+};
